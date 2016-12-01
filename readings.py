ef902c798f37fc3d9b2bc173d0c0a3ee33948601
Brian Rolek <brianrolek@gmail.com>
2016-12-01 14:36:48 -0600

    Add stuff here

--- readings.py
+++ readings.py
@@ -5,9 +5,10 @@ import loaddata
 
 import sys
 
+## Here's just a comment describing what this script does
 def main():
 
-	filename='data/inflammation-01.csv'
+ 	filename='data/inflammation-01.csv'
 
 	data = loaddata.load(filename)
 
